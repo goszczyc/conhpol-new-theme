de040b6fcf6878d43ce732a8a68b58413a432b75
goszczyc <szymon.goszczycki@gmail.com>
2023-09-28 19:17:25 +0200

    minor fixes

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -8,8 +8,6 @@ get_header();
     <?php get_template_part(TMP_PART . 'Banner/banner-slider'); ?>
     <?php get_template_part(TMP_PART . 'about'); ?>
     <?php get_template_part(TMP_PART . 'new-collection'); ?>
-    <h1><?php echo get_the_title(); ?></h1>
-    <?php echo get_the_content(); ?>
 </main>
 
 <?php get_footer(); ?>
