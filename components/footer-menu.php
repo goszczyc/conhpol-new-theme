af6ebb0889500ab63d52e541b3568351491e9834
goszczyc <szymon.goszczycki@gmail.com>
2023-10-14 01:06:11 +0200

    fixed footer

--- components/footer-menu.php
+++ components/footer-menu.php
@@ -8,7 +8,7 @@ $current = ($item['current']) ? '' : '';
 ?>
 <ul class="flex flex-col">
     <?php foreach ($menu['menus'] as $item) : ?>
-        <li class="xs:text-xl leading-[2.125rem] uppercase mb-2">
+        <li class=" leading-none uppercase font-extralight mb-[.6em]">
             <a href="<?php echo $item['url'] ?>" class="hover:text-primary focus:text-primary transition-colors duration-300">
                 <?php echo $item['title'] ?>
             </a>
--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="border-t border-solid border-t-gray">
-    <div class="container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-10 px-5 mx-auto py-[100px]">
+    <div class="container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-10 px-5 mx-auto pt-[60px] max-w-[1102.48px]">
         <div class="col-span-full md:col-span-1">
             <?php if ($social_heading = get_field('social_heading', 'options')) : ?>
 
-                <h3 class="text-xl leading-[2.125rem] font-bold uppercase mb-7 text-center md:text-left">
+                <h3 class="font-extralight leading-none  uppercase mb-[.6em] text-center md:text-left">
                     <?php echo $social_heading; ?>
                 </h3>
 
@@ -18,8 +18,8 @@
                         $icon = get_sub_field('icon');
                     ?>
 
-                        <a href="<?php echo esc_url($url); ?><" class="mr-[22px] last:mr-0">
-                            <?php echo wp_get_attachment_image($icon, 'full', '', ['class' => 'w-12']); ?>
+                        <a href="<?php echo esc_url($url); ?><" class="mr-[14px] last:mr-0">
+                            <?php echo wp_get_attachment_image($icon, 'full', '', ['class' => 'w-10']); ?>
                         </a>
 
                     <?php endwhile; ?>
@@ -38,11 +38,11 @@
             <?php get_template_part(CMP . 'footer-menu', '', ['menu' => 'footer-policies']); ?>
 
         </div>
-        <div class="contact col-span-full sm:col-span-1 sm:text-right">
+        <div class="contact col-span-full sm:col-span-1">
 
             <?php if ($contact_heading = get_field('contact_heading', 'options')) : ?>
 
-                <h3 class="text-xl leading-[2.125rem] font-bold uppercase">
+                <h3 class="font-extralight leading-none mb-[.6em] uppercase">
                     <?php echo $contact_heading; ?>
                 </h3>
 
@@ -50,19 +50,19 @@
 
             <?php if ($contact_info = get_field('contact_info', 'options')) : ?>
 
-                <div class="prose prose-p:leading-[2.125rem] xs:prose-p:text-xl xs:prose-p:leading-[2.125rem] max-w-full ">
+                <div class="prose prose-p:leading-none  xs:prose-p:leading-none max-w-full ">
                     <?php echo $contact_info['address']; ?>
                 </div>
                 <?php if ($contact_info['phone']) : ?>
-                    <div>
-                        <a href="tel: <?php echo $contact_info['phone']; ?>" class="xs:text-xl font-bold hover:text-primary focus:text-primary transition-colors duration-300">
+                    <div class="leading-none">
+                        <a href="tel: <?php echo $contact_info['phone']; ?>" class="xs: font-bold hover:text-primary focus:text-primary transition-colors duration-300">
                             <?php echo $contact_info['phone']; ?>
                         </a>
                     </div>
                 <?php endif; ?>
                 <?php if ($contact_info['email']) : ?>
-                    <div class="div">
-                        <a href="mailto: <?php echo $contact_info['email']; ?>" class="xs:text-xl font-bold hover:text-primary focus:text-primary transition-colors duration-300">
+                    <div class="leading-none">
+                        <a href="mailto: <?php echo $contact_info['email']; ?>" class="xs: font-bold hover:text-primary focus:text-primary transition-colors duration-300">
                             <?php echo $contact_info['email']; ?>
                         </a>
                     </div>
@@ -72,16 +72,16 @@
 
         </div>
     </div>
-    <div class="container px-5 mx-auto">
+    <div class="container px-5 mx-auto mt-8">
         <?php if ($footer_funds = get_field('footer_funds', 'options')) : ?>
 
-            <?php echo wp_get_attachment_image($footer_funds, 'full', '', ['class' => 'w-full max-w-[896px] mx-auto']); ?>
+            <?php wp_get_attachment_image($footer_funds, 'full', '', ['class' => 'w-full max-w-[896px] mx-auto mt-5']); ?>
 
         <?php endif; ?>
 
         <?php if ($company_name = get_field('company_name', 'options')) : ?>
 
-            <div class="text-center text-heading my-8">
+            <div class="text-center text-[30px] leading-8 py-5">
                 <?php echo $company_name; ?>
             </div>
 
@@ -92,10 +92,10 @@
     // Newsletter 
     ?>
     <div class="bg-gray">
-        <div class="container px-5 py-8 mx-auto">
+        <div class="container px-5 mx-auto">
             <?php if ($application_info = get_field('application_info', 'options')) : ?>
 
-                <div class="text-center mb-7">
+                <div class="text-center text-xs font-extralight tracking-[2.2px] p-5 border-b border-solid border-black">
                     <p>
                         <?php echo $application_info['text']; ?>
                         <a href="mailto: <?php echo $application_info['email']; ?>">
@@ -106,24 +106,23 @@
 
             <?php endif; ?>
 
-            <?php if ($newsletter_heading = get_field('newsletter_heading', 'options')) : ?>
+            <div class="py-4 max-w-[980px] mx-auto">
+                <?php if ($newsletter_heading = get_field('newsletter_heading', 'options')) : ?>
 
-                <h2 class="text-heading font-bold upperase text-center uppercase mb-4">
-                    <?php echo $newsletter_heading; ?>
-                </h2>
+                    <h2 class="text-sm font-extralight uppercase text-center leading-none mb-4">
+                        <?php echo $newsletter_heading; ?>
+                    </h2>
 
-            <?php endif; ?>
-            <?php echo do_shortcode('[contact-form-7 id="a7d6e3a" title="Newsletter - nowy"]'); ?>
+                <?php endif; ?>
+                <?php echo do_shortcode('[contact-form-7 id="a7d6e3a" title="Newsletter - nowy"]'); ?>
+            </div>
         </div>
     </div>
 
-    <div class="container flex justify-between items-center px-5 py-6 mx-auto">
-        <div class="text-gray text-sm">
-            <?php _e('All rights reserved'); ?> &copy;CONHPOL <?php echo date('Y'); ?>
+    <div class="container flex justify-center items-center px-5 py-4 mx-auto">
+        <div class="text-gray text-[10px] leading-none text-center">
+             &copy; <?php echo date('Y'); ?> Conhpol <?php _e('All rights reserved'); ?>
         </div>
-        <a href="https://everywhere.pl/">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/everywhere-logo.png" alt="Everywhere Logo" class="w-[70px]">
-        </a>
     </div>
 
 </footer>
